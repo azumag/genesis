1d2ff0bf56a5a19c18ca45904ace2e95759af237
azumag <tsubasa.azumagakito@gmail.com>
2019-08-30 20:01:48 +0900

    fix

--- genesis.rb
+++ genesis.rb
@@ -1,33 +1,68 @@
 
-#神は光あれと言った　すると光があった
+# 神は光あれと言った　すると光があった
 class Light 
 end
 
-#神は光を昼と名づけ、やみを夜と名づけられた。夕となり、また朝となった。第一日である。
+# 神は光を昼と名づけ、やみを夜と名づけられた。夕となり、また朝となった。第一日である。
 noon = Light.new
 night = nil
 
 
-#神はまた言われた、「水の間におおぞらがあって、水と水とを分けよ」。
+# 神はまた言われた、「水の間におおぞらがあって、水と水とを分けよ」。
 class Water 
 end
 
 class Air
 end
 
-# 2-dim array world
-sea = 10.times.map { Water.new }
-sky = 10.times.map { Air.new }
-cloud = 10.times.map { rand < 0.5 ? Air.new : Water.new }
+class Cloud
+  def initialize
+    component = [ 7.times.map { rand < 0.5 ? Air.new : Water.new } ]
+  end
+end
+
+# 3-dim array world
+def make_field
+  10.times.map { 10.times.map { yield } }
+end
+
+sea = make_field { Water.new }
+sky = make_field { Air.new }
+cloud = make_field { rand < 0.5 ? Air.new : Water.new }
 
 world = [cloud, sky, sea]
 
 pp world
+exit
 
-#神はまた言われた、「天の下の水は一つ所に集まり、かわいた地が現れよ」
-#神はそのかわいた地を陸と名づけ、水の集まった所を海と名づけられた。
+# 神はまた言われた、「天の下の水は一つ所に集まり、かわいた地が現れよ」
+# 神はそのかわいた地を陸と名づけ、水の集まった所を海と名づけられた。
 class Soil
 end
 
-world[2] = 10.times.map { rand < 0.5 ? sea.choice : Soil.new }
+world[2] = 10.times.map { rand < 0.5 ? sea.sample : Soil.new }
 
+# 神はまた言われた、
+# 「地は青草と、種をもつ草と、
+# 種類にしたがって種のある実を結ぶ果樹とを地の上にはえさせよ」
+class GrassSeed
+end
+
+class Grass
+  seeds = []
+  def make_seed
+    seeds << GrassSeed.new
+  end
+end
+
+class Seed end
+class Fruit 
+  seeds = 10.times.map { Seed.new }
+end
+
+class Tree
+  fruits = []
+  def make_fruits
+    fruits << Fruit.new
+  end
+end
